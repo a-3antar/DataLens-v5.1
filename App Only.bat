d7155a8d54a7ad37e489a1d6a957428623dcc071
a-3antar <a.3antar@gmail.com>
2026-08-27 01:30:34 +0300

    Create App Only.bat

---
+++ App Only.bat
@@ -0,0 +1,15 @@
+@echo off
+cd /d "%~dp0"
+
+:: Start Ollama with Yellow text (color code 0A)
+:: echo [Ollama] Starting in Green...
+
+:: start /b "" cmd /c "ollama.exe serve"
+color 0A
+:: Wait for Ollama to initialize
+timeout /t 3 /nobreak >nul
+
+:: Start Streamlit with CYAN text (color code 0B) in the foreground
+echo [Streamlit] Starting in Cyan...
+color 0B
+streamlit run "E:\Work\Data Analysis\DataLens v5.1\main.py"
